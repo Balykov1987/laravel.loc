968275950148595e78fe6382bf3b563b76a43d5e
Anton <balykov1987@gmail.com>
2020-11-10 12:23:32 +0400

    Init

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+
+	Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo obcaecati adipisci quis quod a quidem recusandae, quo facere nobis dolores incidunt maiores et minus! Porro perspiciatis, impedit dolor vero.	
+
+</footer>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+
+
+@section('content')
+	
+	<p>Hello</p>
+	
+@endsection
